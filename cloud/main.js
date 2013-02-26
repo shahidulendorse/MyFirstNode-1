a443a9c40843c7de0bd3ed8cb64db33c9a10c4af
shahidul.hoque.57@gmail.com <shahidul.hoque.57@gmail.com>
2013-02-26 12:11:29 +0000

    update file /cloud/main.js

--- cloud/main.js
+++ cloud/main.js
@@ -7,10 +7,12 @@ exports.myFunction = function(params, callback) {
 };
 
 exports.myFunction1 = function(params, callback) {
+   var num = params.num;
   callback(undefined, {result: num});
 };
 
 exports.myFunction2 = function(params, callback) {
+   var num = params.num;
   return callback({err: 'Num too large'}, {result: num});
 };
 
